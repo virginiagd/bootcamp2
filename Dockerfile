2fab2681aab45d70ab9fda7bc7679d981a8027a3
Jotta03 <95530406+Jotta03@users.noreply.github.com>
2024-10-09 09:05:25 +0200

    Eliminada basura

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,8 @@ WORKDIR /app
 # Copiar el directorio actual al contenedor en /app
 COPY . .
 
-COPY requirements.txt requirements.txt
-
 # Instalar requisitos
 RUN pip install --no-cache-dir -r requirements.txt
-
-# Copia el script de entrada
-COPY entrypoint.sh .
 
 # Establece el script como el comando de inicio
 CMD ["./entrypoint.sh"]
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -14,19 +14,6 @@ def create_app(test_config=None):
     app.config['DB_NAME'] = os.getenv("MYSQL_DATABASE")
     app.secret_key = os.getenv("SECRET_KEY")
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-    
     from . import db
     db.init_app(app)
 
